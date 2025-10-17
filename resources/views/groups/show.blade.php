ee3a57d75be25f4be296c4c28f29a73ead31f934
eline <1096534@hr.nl>
2025-10-17 18:23:25 +0200

    ADD agency table, model, controller

--- resources/views/groups/show.blade.php
+++ resources/views/groups/show.blade.php
@@ -1,6 +1,8 @@
 <x-app-layout>
     <h1> {{ $group->name }}</h1>
     <p>{{$group->type->type}}</p>
+    <p>{{$group->agency->name}}</p>
+
     <p>{{$group->debut_date}}</p>
     <p>{{$group->number_of_members}}</p>
     <p>{{$group->name_of_members}}</p>
